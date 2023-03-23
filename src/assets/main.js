40fc90fb2308fe66f0ef2315582b93dc5f1a1407
Paulo Galarza <paulogalarza1993@gmail.com>
2023-03-23 15:57:22 -0500

    Modificacion HTML

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,7 @@
 const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCoGDh1Xa3kUCpok24JN5DKA&part=snippet%2Cid&order=date&maxResults=5';
 
+const content = null || document.getElementById('content');
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -17,26 +19,19 @@ async function fetchData(urlApi) {
 (async () => {
 try {
     const videos = await fetchData(API);
-} catch (error) {
-    let view = `
-    ${videos.items.map(videos =>
-        <div class="group relative">
-            <div
-            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-            <img src="${videos.snippet.thumbnail.height.url}" alt="${videos.snippet.description}" class="w-full">
-            </div>
-            <div class="mt-4 flex justify-between">
-                <h3 class="text-sm text-gray-700">
-                    <span aria-hidden="true" class="absolute inset-0"></span>
-                    ${videos.snippet.title}
-                    title
-                </h3>
-            </div>
+    let view=`
+    ${videos.items.map(video=>`
+      <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
         </div>
-    `).silece(0,4).join('')};
-})();
-
-fetch('', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+    `).slice(0,4).join('')}
+    `;content.innerHTML=view;}catch(error){console.log(error);}})();
